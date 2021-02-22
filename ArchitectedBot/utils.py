bf098278039a507a1238d851a154b60443efb11c
Joshua Cantrell <joshuacantrell@gmail.com>
2021-02-21 20:12:33 -0600

    Added Files

---
+++ ArchitectedBot/utils.py
@@ -0,0 +1,36 @@
+import pytesseract, os, cv2
+import numpy as np
+from difflib import SequenceMatcher
+from PIL import Image
+
+
+def was_killed_by_archetects(img_name) -> bool:
+    img = cv2.imread(img_name)
+    original = img
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    gray = cv2.bitwise_not(img_bin)
+    kernel = np.ones((2, 1), np.uint8)
+    img = cv2.erode(gray, kernel, iterations=1)
+    img = cv2.dilate(img, kernel, iterations=1)
+    #cv2.imshow("img", img)
+    for im in [img, original, Image.open(img_name)]:
+        string = pytesseract.image_to_string(im, timeout=3)
+        for st in string.splitlines():
+            split_str = st.split(" ")
+            for index in range(0, len(split_str)//4+1):
+                match_string = " ".join(split_str[index:index+4])
+                matcher = SequenceMatcher(lambda x: x==" ", "Killed by the archetects".lower(), match_string.lower()) 
+                #print(matcher.ratio())
+                #print(match_string)
+                if matcher.ratio() > .8:
+                    return True
+    
+
+
+if __name__ == "__main__":
+    directory = "C:\\Users\\joshu\\Desktop\\Architected\\"
+    #was_killed_by_archetects(cv2.imread(directory+"difficult.png"))
+    
+    for file in os.listdir(directory):
+        print(was_killed_by_archetects(directory+file))
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="ArchitectedBot",
+    version="0.0.1",
+    author="Joshua",
+    author_email="joshua@piethon.party",
+    description="architected bot",
+    long_description="",
+    long_description_content_type="",
+    url="",
+    install_requires=["discord.py", "numpy", "opencv-python", "pytesseract"]
+)
